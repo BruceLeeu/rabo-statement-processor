13938edbe8a1937906b7ef99cc7e8afaa6a85d85
Louis la Grange <louis@louislagrange.co.za>
2025-09-21 14:59:13 +0200

    Remove unused imports

--- statement-processor/src/main/kotlin/rabo/statementprocessor/Controller.kt
+++ statement-processor/src/main/kotlin/rabo/statementprocessor/Controller.kt
@@ -1,14 +1,10 @@
 package rabo.statementprocessor
 
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 import rabo.statementprocessor.exceptions.InvalidFileTypeException
 import rabo.statementprocessor.types.Statement
 import rabo.statementprocessor.types.StatementResponse
